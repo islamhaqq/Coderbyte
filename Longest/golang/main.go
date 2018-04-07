6b2d1ea4072cbbb0f08756276bf1e8e154ac305e
Syed Aman <sna274@nyu.edu>
2018-04-07 08:02:06 -0500

    solve the problem using golang

--- Longest/golang/main.go
+++ Longest/golang/main.go
@@ -1,8 +1,28 @@
 package main
 import "fmt"
+import "strings"
+import "regexp"
 
+// Sanitize the string of punctuation.
+func sanitizeString(theString string) string {
+  alphaRegex, _ := regexp.Compile(`[^\w\s]+`)
+  return alphaRegex.ReplaceAllString(theString, "")
+}
+
+// Get the longest word in a sentence.
 func LongestWord(sen string) string{
-  return sen
+  // Get the words of the sentence.
+  words := strings.Split(sanitizeString(sen), " ")
+
+  // Determine the longest word amongst those words.
+  longestWord := words[0]
+  for _, word :=range words {
+    if len(word) > len(longestWord) {
+      longestWord = word
+    }
+  }
+
+  return longestWord
 }
 
 func main() {
