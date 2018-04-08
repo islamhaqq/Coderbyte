5d5f4154ba753b59808061eefb22c480957993e0
Syed Aman <sna274@nyu.edu>
2018-04-08 07:41:44 -0500

    improve factorial tests

--- Factorial/golang/main_test.go
+++ Factorial/golang/main_test.go
@@ -7,4 +7,12 @@ func TestFactorial(t *testing.T) {
   if (Factorial(5) != 120) {
     t.Errorf("Expected Factorial(%d) to be %d, but got %d", 5, 120, Factorial(5))
   }
+
+  if (Factorial(1) != 1) {
+    t.Errorf("Expected Factorial(%d) to be %d, but got %d", 1, 1, Factorial(1))
+  }
+
+  if (Factorial(15) != 1307674368000) {
+    t.Errorf("Expected Factorial(%d) to be %d, but got %d", 15, 1307674368000, Factorial(15))
+  }
 }
