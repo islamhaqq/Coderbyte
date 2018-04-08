115d726cc3b722f0b9520c50d2241f458fcd0cef
Syed Aman <sna274@nyu.edu>
2018-04-08 07:39:38 -0500

    add comments

--- Factorial/golang/main_test.go
+++ Factorial/golang/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import "testing"
 
+// Unit test Factorial().
 func TestFactorial(t *testing.T) {
   if (Factorial(5) != 120) {
     t.Errorf("Expected Factorial(%d) to be %d, but got %d", 5, 120, Factorial(5))
