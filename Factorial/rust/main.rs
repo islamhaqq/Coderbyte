962840af8438de84c09197e12248f64b710e6b0d
Syed Aman <sna274@nyu.edu>
2018-04-11 08:53:22 -0500

    Update main.rs

--- Factorial/rust/main.rs
+++ Factorial/rust/main.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("{}", factorial(5));
 }
 
-// Gets the factorial of a number.
+// Gets the factorial of a number using the !n = n * !(n-1) algorithm.
 fn factorial(num: u32) -> u32 {
-    return if num == 1 { 1 } else { num * factorial(num - 1) };
+    return if num > 1  { num * factorial(num - 1) } else {1};
 }
