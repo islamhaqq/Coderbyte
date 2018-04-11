9a9860411d66247e4de31b498e1d297bbdd717fa
Syed Aman <sna274@nyu.edu>
2018-04-11 08:51:15 -0500

    Update index.js

--- Factorial/javascript/index.js
+++ Factorial/javascript/index.js
@@ -1,4 +1,4 @@
 // Calculates the factorial of a number recursively using the: !n = n * !(n-1) algorithm.
-const factorial = n => n === 1 ? 1 : n * factorial(n-1);
+const factorial = n => n > 1 ? n * factorial(n-1) : 1;
 
 module.exports = factorial;
