0bf8a5e48eb7684971d40edf3f8ae92bb66633f0
Syed Aman <sna274@nyu.edu>
2018-04-12 09:58:36 -0500

    Update index.test.js

--- First-Reverse/javascript/index.test.js
+++ First-Reverse/javascript/index.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
-const firstReverse = require('./');
+const FirstReverse = require('./');
 
-assert.equal(firstReverse('javascript'), 'tpircsavaj');
-assert.equal(firstReverse('nodejs'), 'sjedon');
-assert.equal(firstReverse('es6'), '6se');
+assert.equal(FirstReverse('javascript'), 'tpircsavaj');
+assert.equal(FirstReverse('nodejs'), 'sjedon');
+assert.equal(FirstReverse('es6'), '6se');
