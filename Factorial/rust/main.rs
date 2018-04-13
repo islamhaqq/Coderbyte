b25dff163050ef4975ea643f40dfa31c81e05cec
Syed Aman <sna274@nyu.edu>
2018-04-13 17:26:35 -0500

    create unit tests for the factorial rust solution

--- Factorial/rust/main.rs
+++ Factorial/rust/main.rs
@@ -6,3 +6,20 @@ fn main() {
 fn factorial(num: u32) -> u32 {
     return if num > 1  { num * factorial(num - 1) } else {1};
 }
+
+// Define unit tests.
+#[cfg(test)]
+mod tests {
+    use factorial;
+
+    #[test]
+    fn it_gets_the_factorial() {
+        assert_eq!(factorial(0), 1);
+        assert_eq!(factorial(1), 1);
+        assert_eq!(factorial(2), 2);
+        assert_eq!(factorial(3), 6);
+        assert_eq!(factorial(4), 24);
+        assert_eq!(factorial(5), 120);
+        assert_eq!(factorial(6), 720);
+    }
+}
